57fde5e2220e579f789936e437ae748f5c318f68
DESKTOP-PV45JUU\I3A <tpeleato@unizar.es>
2023-02-20 11:21:39 +0100

    dev

--- Web/js/events.js
+++ Web/js/events.js
@@ -78,7 +78,6 @@ let mybutton = document.getElementById("myBtn");
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  alert('entroscrollFunction')
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     mybutton.style.display = "block";
   } else {
